13ab410d221dcdb29cc669a2779359096bb37d4e
Thomas Rohleder <tcr@netcompany.com>
2020-06-10 12:28:06 +0200

    cleanup server file

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(' GET World');
 });
 
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
+app.post('/',(req, res) => {
+  res.send(' POST World');
 });
 
+app.put('/', (req, res) => {
+  res.send(' PUT World');
+});
+
+app.delete('/', (req, res) => {
+  res.send(' DELETE World');
+});
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
