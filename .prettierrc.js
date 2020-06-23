5de2790057cdfcec7d713483c331a981d0bd0c4d
Troels Beck Krøgh <tbk@netcompany.com>
2020-06-23 12:45:35 +0200

    IOT-72: Prettier for automated formatting

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: "always",
+};
