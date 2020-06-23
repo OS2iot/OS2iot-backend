e9f2ee981c746370ae84a7217dda7beee2feafab
Troels Beck Krøgh <tbk@netcompany.com>
2020-06-23 12:36:25 +0200

    IOT-72: Extract functionality in usercontroller such that it can be tested

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,5 +1,4 @@
 import { Router, Request, Response } from 'express';
-import middlewares from '../middlewares';
 import Logger from '../../loaders/logger';
 import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../../repositoies/UserRepository';
@@ -10,30 +9,33 @@ const route = Router();
 export default (app: Router): void => {
   app.use('/users', route);
 
-  route.get('/me', middlewares.isAuth, (req: Request, res: Response) => {
-    return res.json({ user: 'asdf' }).status(200);
-  });
+  route.get('/:userId', getUserById());
+  route.get('/', getAll());
+}
 
-  route.get('/:userId', (req: Request, res: Response) => {
-    const userId = Number(req.params['userId'])
+function getUserById() {
+  return (req: Request, res: Response) => {
+    const userId = Number(req.params['userId']);
 
     const userRepository = getCustomRepository(UserRepository);
 
     userRepository.findById(userId).then(result => {
-      Logger.info(result.toString())
+      Logger.info(result.toString());
       return res.json({ user: result }).status(200);
     }).catch(err => {
-      Logger.error(err)
-      return res.status(404).json({ error: `No user found for id: ${req.params['userId']}` })
-    })
-  });
+      Logger.error(err);
+      return res.status(404).json({ error: `No user found for id: ${req.params['userId']}` });
+    });
+  };
+}
 
-  route.get('/', (req: Request, res: Response) => {
+function getAll() {
+  return (req: Request, res: Response) => {
     const userRepository = getCustomRepository(UserRepository);
     userRepository.findAll().then(result => {
-      return res.json({users: result}).status(200);
+      return res.json({ users: result }).status(200);
     }).catch(err => {
-      throw err
-    })
-  });
+      throw err;
+    });
+  };
 }
