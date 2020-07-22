7a4592fe075e0bed14b28b8ae7fe116f5b15ca8d
tcrnetcomp <56109715+tcrnetcomp@users.noreply.github.com>
2020-07-22 14:17:45 +0200

    fix generic http integration - msg can be recieved now

--- src/admin-controller/iot-device.controller.ts
+++ src/admin-controller/iot-device.controller.ts
@@ -38,19 +38,6 @@ export class IoTDeviceController {
         return application;
     }
 
-    @Get(":apiKey")
-    @ApiOperation({ summary: "Find one IoT-Device by apiKey" })
-    @ApiNotFoundResponse()
-    async findOneByApiKey(@Param("apiKey") apiKey: string): Promise<IoTDevice> {
-        try {
-            const key = await this.iotDeviceService.findOneByApiKey(apiKey);
-            return key;
-        } catch (err) {
-
-       
-    
-        }
-    }
     @Get(":id")
     @ApiOperation({ summary: "Find one IoT-Device by id" })
     @ApiNotFoundResponse()
@@ -59,6 +46,18 @@ export class IoTDeviceController {
             return await this.iotDeviceService.findOne(id);
         } catch (err) {
             throw new NotFoundException(`No element found by id: ${id}`);
+        }
+    }
+
+    @Get("nyApiKey/:apiKey")
+    @ApiOperation({ summary: "Find one IoT-Device by apiKey" })
+    @ApiNotFoundResponse()
+    async findOneByApiKey(@Param("apiKey") apiKey: string): Promise<boolean> {
+        try {
+            const key = await this.iotDeviceService.findOneByApiKey(apiKey);
+            return true;
+        } catch (err) {
+            return false;
         }
     }
 
