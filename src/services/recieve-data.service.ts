a570f7ee4039c17121236eeb0511ec51d0e1d07a
tcrnetcomp <56109715+tcrnetcomp@users.noreply.github.com>
2020-07-22 14:43:17 +0200

    create recieveData service

--- src/services/recieve-data.service.ts
+++ src/services/recieve-data.service.ts
@@ -3,18 +3,32 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { RecieveData } from "@entities/recieve-data.entity";
 import { Repository } from "typeorm";
 import { CreateRecieveDataDto } from "@dto/create-recieve-data.dto";
+import { IoTDeviceService } from "./iot-device.service";
 
 @Injectable()
 export class RecieveDataService {
     constructor(
+        private iotDeviceService: IoTDeviceService,
+
         @InjectRepository(RecieveData)
         private recieveDataRepository: Repository<RecieveData>
     ) {}
 
-    async create(): Promise<RecieveData> {
-        const recieveData = new RecieveData();
-        //TODO: inser Logger returning Enum;
-        Logger.log(recieveData.toString);
-        return this.recieveDataRepository.save(recieveData);
+    async create(apiKey: string, data: string): Promise<string> {
+        try {
+            const device = await this.iotDeviceService.findOneByApiKey(apiKey);
+
+            try {
+                JSON.parse(data);
+            } catch (e) {
+                return "406 Not Acceptable";
+            }
+
+            if (!device) return "403 Forbidden";
+            //TODO: 204 No Content - når recievedData er videresendt
+            if (device) return " 204 No Content";
+        } catch (e) {
+            throw e;
+        }
     }
 }
