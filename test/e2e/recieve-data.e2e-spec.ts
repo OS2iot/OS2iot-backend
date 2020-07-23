cf787718ac3de1266691844c5cddbf14996a9350
tcrnetcomp <56109715+tcrnetcomp@users.noreply.github.com>
2020-07-23 16:06:33 +0200

    Implement e2e test for recieve data
    This has revealed that I hvae introduced a bug, such that recieve-data returns  "status": 403, Fobidden" no matter what.
    
    I will have to look into this friday

--- test/e2e/recieve-data.e2e-spec.ts
+++ test/e2e/recieve-data.e2e-spec.ts
@@ -2,25 +2,24 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication, Logger } from "@nestjs/common";
 import * as request from "supertest";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { RecieveDataModule } from "@modules/recieve-data.module";
 import { Repository, getManager } from "typeorm";
-import { RecieveData } from "@entities/recieve-data.entity";
-import { clearDatabase } from "./test-helpers";
-import { Application } from "@entities/application.entity";
-import { ApplicationModule } from "@modules/application.module";
+import { IoTDevice } from "@entities/iot-device.entity";
+import { IoTDeviceModule } from "@modules/iot-device.module";
 import { GenericHTTPDevice } from "@entities/generic-http-device.entity";
+import { clearDatabase } from "./test-helpers";
+import { response } from "express";
+import { RecieveDataModule } from "@modules/recieve-data.module";
 
 describe("RecieveDataController (e2e)", () => {
     let app: INestApplication;
-    let recieveDataRepository: Repository<RecieveData>;
-    let ioTDeviceRepository: Repository<IoTDevice>;
+    let repository: Repository<IoTDevice>;
+    let genericHTTPDeviceRepository: Repository<GenericHTTPDevice>;
 
-    let applicationRepository: Repository<Application>;
+    let applicationRepository: Repository<IoTDevice>;
 
     beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [
-                ApplicationModule,
                 RecieveDataModule,
                 TypeOrmModule.forRoot({
                     type: "postgres",
@@ -30,7 +29,7 @@ describe("RecieveDataController (e2e)", () => {
                     password: "toi2so",
                     database: "os2iot-e2e",
                     synchronize: true,
-                    logging: false,
+                    logging: true,
                     autoLoadEntities: true,
                 }),
             ],
@@ -39,8 +38,13 @@ describe("RecieveDataController (e2e)", () => {
         app = moduleFixture.createNestApplication();
         await app.init();
 
-        // Get a reference to the recieveDataRepository such that we can CRUD on it.
-        recieveDataRepository = moduleFixture.get("RecieveDataRepository");
+        // Get a reference to the repository such that we can CRUD on it.
+        repository = moduleFixture.get("IoTDeviceRepository");
+        genericHTTPDeviceRepository = moduleFixture.get(
+            "GenericHTTPDeviceRepository"
+        );
+
+        applicationRepository = moduleFixture.get("ApplicationRepository");
     });
 
     afterAll(async () => {
@@ -49,67 +53,16 @@ describe("RecieveDataController (e2e)", () => {
     });
 
     beforeEach(async () => {
-        // Clear data before each test
         await clearDatabase();
     });
 
     afterEach(async () => {
-        // Clear data after each test
         await clearDatabase();
     });
-    it("(POST) /application/ - Create application", async () => {
-        const testAppOne = { name: "Post Test", description: "Post Tester" };
 
-        await request(app.getHttpServer())
-            .post("/application/")
-            .send(testAppOne)
-            .expect(201)
-            .expect("Content-Type", /json/)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    name: "Post Test",
-                    description: "Post Tester",
-                    iotDevices: [] as any[],
-                });
-            });
-
-        const applicationInDatabase: Application[] = await applicationRepository.find();
-        expect(applicationInDatabase).toHaveLength(1);
-    });
-
-    it("(POST) /iot-device/", async () => {
+    it("(POST) /recieve-data/", async () => {
+        // Insert data with correct API key - expected 400
         const applications = await applicationRepository.save([
-            { name: "Test", description: "Tester", iotDevices: [] },
-        ]);
-        const appId = applications[0].id;
-        const testIoTDevice = {
-            name: "created",
-            type: "GENERIC_HTTP",
-            applicationId: appId,
-            comment: "string",
-        };
-        return await request(app.getHttpServer())
-            .post("/iot-device/")
-            .send(testIoTDevice)
-            .expect(201)
-
-            .expect("Content-Type", /json/)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    name: "created",
-                    type: "GENERIC_HTTP",
-                    application: {
-                        id: appId,
-                    },
-                    comment: "string",
-                    location: null,
-                    commentOnLocation: null,
-                });
-            });
-    });
-
-    it("(GET) /iot-device/:id - one", async () => {
-        const applications = await ioTDeviceRepository.save([
             { name: "Test", description: "Tester", iotDevices: [] },
         ]);
         const appId = applications[0].id;
@@ -117,119 +70,80 @@ describe("RecieveDataController (e2e)", () => {
         const device = new GenericHTTPDevice();
         device.name = "HTTP device";
         device.application = applications[0];
-        device.apiKey = "asdf";
+        // @Hack: to call beforeInsert (private)
+        (device as any).beforeInsert();
 
         const manager = getManager();
-        const iotDevice = await manager.save(device);
+        const savedIoTDevice = await manager.save(device);
 
-        const iotDeviceId = iotDevice.id;
+        const oldUuid = savedIoTDevice.apiKey;
+
+        const changedIoTDeviceJson = {
+            name: "changed",
+            type: "GENERIC_HTTP",
+            applicationId: appId,
+            comment: "new comment",
+        };
+
         return await request(app.getHttpServer())
-            .get("/iot-device/" + iotDeviceId)
-            .expect(200)
-
-            .expect("Content-Type", /json/)
-            .then(response => {
-                // console.log(response.body);
-                // TODO: get apiKey out of body and pass it to the next text
-                expect(response.body).toMatchObject({
-                    name: "HTTP device",
-                    application: {
-                        id: appId,
-                    },
-                });
-            });
+            .post("/recieve-data/?apiKey=" + oldUuid)
+            .send(changedIoTDeviceJson)
+            .expect(200);
     });
 
-    //TODO:1. Skriv test hvor apiKey er forkert
-    it("(POST) /recieveData/ - Create application", async () => {
-        const testAppOne = {
-            apiKey: "a087911a-dc19-440d-94ff-72cc40cc8a69",
-            body: '{"TestData":1}',
-        };
-        /**
-             {
-             "statusCode": 400,
-            "message": "Unexpected token s in JSON at position 0",
-            "error": "Bad Request"
-            }
-        */
-        await request(app.getHttpServer())
-            .post("/recieveData/")
-            .send(testAppOne)
-            .expect(201)
-            .expect("Content-Type", /json/)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    response: {
-                        status: 204,
-                        error: "204 No Content",
-                        description: "204 No Content",
-                    },
-                    status: 204,
-                    message: "Http Exception",
-                });
-            });
+    it("(POST) /recieve-data/", async () => {
+        // Insert data with correct API key libut invd data - expected 400
+        const applications = await applicationRepository.save([
+            { name: "Test", description: "Tester", iotDevices: [] },
+        ]);
+        const appId = applications[0].id;
+
+        const device = new GenericHTTPDevice();
+        device.name = "HTTP device";
+        device.application = applications[0];
+        // @Hack: to call beforeInsert (private)
+        (device as any).beforeInsert();
+
+        const manager = getManager();
+        const savedIoTDevice = await manager.save(device);
+        //  Logger.log(savedIoTDevice);
+
+        const oldUuid = savedIoTDevice.apiKey;
+
+        return await request(app.getHttpServer())
+            .post("/recieve-data/?apiKey=" + oldUuid)
+            .send("data string is invalid")
+            .expect(400);
     });
-    //TODO: 2. Skriv test hvor data er formaeret forkert(invalid json)
-    it("(POST) /recieveData/ - Create application", async () => {
-        const testAppOne = {
-            apiKey: "a087911a-dc19-440d-94ff-72cc40cc8a69",
-            body: '{"TestData"1',
+
+    it("(POST) /recieve-data/", async () => {
+        // Insert invalid API key - expected 403- fobbidden
+        const applications = await applicationRepository.save([
+            { name: "Test", description: "Tester", iotDevices: [] },
+        ]);
+        const appId = applications[0].id;
+
+        const device = new GenericHTTPDevice();
+        device.name = "HTTP device";
+        device.application = applications[0];
+        // @Hack: to call beforeInsert (private)
+        (device as any).beforeInsert();
+
+        const manager = getManager();
+        const savedIoTDevice = await manager.save(device);
+
+        const oldUuid = savedIoTDevice.apiKey;
+
+        const changedIoTDeviceJson = {
+            name: "changed",
+            type: "GENERIC_HTTP",
+            applicationId: appId,
+            comment: "new comment",
         };
-        /*
-                {   
-                    //Når device apiKey er forkert
-                    status: HttpStatus.FORBIDDEN,
-                    error: "403 Forbidden",
-                    description: "403 Forbidden",
-                }
-            */
-        await request(app.getHttpServer())
-            .post("/recieveData/")
-            .send(testAppOne)
-            .expect(201)
-            .expect("Content-Type", /json/)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    response: {
-                        status: 204,
-                        error: "204 No Content",
-                        description: "204 No Content",
-                    },
-                    status: 204,
-                    message: "Http Exception",
-                });
-            });
-    });
-    //TODO:3. Skriv test hvor data og apiKey er korrekt
-    it("(POST) /recieveData/ - Create application", async () => {
-        const testAppOne = {
-            apiKey: "a087911a-dc19-440d-94ff-72cc40cc8a69",
-            body: '{"TestData":1}',
-        };
-        /*
-            {
-                //return "204 No Content";
-                status: HttpStatus.NO_CONTENT,
-                error: "204 No Content",
-                description:  "204 No Content",
-            },
-         */
-        await request(app.getHttpServer())
-            .post("/recieveData/")
-            .send(testAppOne)
-            .expect(201)
-            .expect("Content-Type", /json/)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    response: {
-                        status: 204,
-                        error: "204 No Content",
-                        description: "204 No Content",
-                    },
-                    status: 204,
-                    message: "Http Exception",
-                });
-            });
+
+        return await request(app.getHttpServer())
+            .post("/recieve-data/?apiKey=" + "invalidKey")
+            .send(changedIoTDeviceJson)
+            .expect(403);
     });
 });
