4fb1a03466dbda9d78f6068311cfc815e23ea463
Troels Beck Krøgh <tbk@netcompany.com>
2020-07-30 14:52:49 +0200

    Cleanup lint

--- test/unit/data-target.controller.spec.ts
+++ test/unit/data-target.controller.spec.ts
@@ -1,8 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { DataTargetController } from "@admin-controller/data-target.controller";
 import { DataTargetService } from "@services/data-target.service";
-import { DataTarget } from "@entities/data-target.entity";
-import { getRepositoryToken } from "@nestjs/typeorm";
 import { DataTargetType } from "../../src/entities/enum/data-target-type.enum";
 
 describe("DataTarget Controller", () => {
