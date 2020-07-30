0928feaeed858eb080b92b4d4fee3e6794726f57
tcrnetcomp <56109715+tcrnetcomp@users.noreply.github.com>
2020-07-30 13:33:36 +0200

    add device does not exist message

--- src/entities/enum/error-codes.enum.ts
+++ src/entities/enum/error-codes.enum.ts
@@ -2,4 +2,5 @@ export enum ErrorCodes {
     IdDoesNotExists = "MESSAGE.ID-DOES-NOT-EXIST",
     IdMissing = "MESSAGE.ID-MISSING-FROM-REQUEST",
     NameInvalidOrAlreadyInUse = "MESSAGE.NAME-INVALID-OR-ALREADY-IN-USE",
+    InvalidApiKey = "MESSAGE.DEVICE-DOES-NOT-EXIST",
 }
