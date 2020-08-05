d7ce06d363e66ddf523e401265cb72d2e75936c1
Thomas Rohleder <tcr@netcompany.com>
2020-08-05 16:55:30 +0200

    IOT-88: Working on e2e-test - next step -> spin up docker chirpstack for test
    IOT-88: getCountNetworkServer -> getNetworkServerCount
    
    IOT-88: fixed bug I  accidently introduced in getNetworkservers
    
    IOT-88: Add network-server dto's and implement in network-server service
    
    IOT-88: Temporaroly pause e2e test

--- src/services/chirpstack/chirpstack-network-server.service.ts
+++ src/services/chirpstack/chirpstack-network-server.service.ts
@@ -1,20 +1,54 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { Injectable, OnModuleInit, Logger } from "@nestjs/common";
 
 import { GenericChirpstackConfigurationService } from "./generic-chirpstack-configuration.service";
 import { CreateNetworkServerDto } from "@dto/create-network-server.dto";
+import { ListAllNetworkServerReponseDto } from "@dto/list-all-network-server-response.dto";
+import { UpdateNetworkServerDto } from "@dto/update-network-server.dto";
+import { DeleteResponseDto } from "@dto/delete-application-response.dto";
 
 @Injectable()
 export class ChirpstackSetupNetworkServerService
     extends GenericChirpstackConfigurationService
     implements OnModuleInit {
     async onModuleInit(): Promise<void> {
+        if ((await this.getNetworkServerCount()) < 1) {
+            this.postNetworkServer(this.setupData());
+        }
+    }
+
+    public async postNetworkServer(
+        data: string
+    ): Promise<CreateNetworkServerDto> {
+        return await this.post("network-servers", data);
+    }
+    public async putNetworkServer(
+        data: string,
+        id: number
+    ): Promise<UpdateNetworkServerDto> {
+        return await this.put("network-servers", data, id);
+    }
+    public async deleteNetworkServer(id: number): Promise<DeleteResponseDto> {
+        Logger.error("Delete " + id);
+        return await this.delete("network-servers", id);
+    }
+    public async getNetworkServer(
+        limit?: number,
+        offset?: number
+    ): Promise<ListAllNetworkServerReponseDto> {
+        return await this.getAll("network-servers", limit, offset);
+    }
+    public async getNetworkServerCount(): Promise<number> {
+        return await this.getCount("network-servers");
+    }
+
+    setupData(): string {
         const chirpstackNetworkServerName =
             "os2iot-docker_chirpstack-network-server_1:8000";
-        const endpoint = "network-servers";
+
         const createNetworkServerDto: CreateNetworkServerDto = {
             name: chirpstackNetworkServerName,
             server: chirpstackNetworkServerName,
-
+            /*
             caCert: "",
             gatewayDiscoveryDR: 0,
             gatewayDiscoveryEnabled: false,
@@ -25,12 +59,11 @@ export class ChirpstackSetupNetworkServerService
             routingProfileTLSKey: "",
             tlsCert: "",
             tlsKey: "",
+            */
         };
         const data: string = //TODO: skriv om til at bruge en DTO
             '{"networkServer":' + JSON.stringify(createNetworkServerDto) + "}";
 
-        if ((await this.getCount(endpoint)) < 1) {
-            this.post(endpoint, data); //Måske er det i virkeligheden ligegyldigt om den findes i forvejen. ??
-        }
+        return data;
     }
 }
