b7ca1f5bfd04bef34f5b933f3bc631e55baeeca0
Thomas Rohleder <tcr@netcompany.com>
2020-08-05 09:10:46 +0200

    IOT-88: implement network-server dto

--- src/services/chirpstack/setup-network-server/chirpstack-network-server.service.ts
+++ src/services/chirpstack/setup-network-server/chirpstack-network-server.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit, HttpService } from "@nestjs/common";
+import { Injectable, OnModuleInit } from "@nestjs/common";
 
 import { GenericChirpstackConfigurationService } from "../generic-chirpstack-configuration/generic-chirpstack-configuration.service";
 import { CreateNetworkServerDto } from "@dto/create-network-server.dto";
@@ -11,12 +11,23 @@ export class ChirpstackSetupNetworkServerService
         const chirpstackNetworkServerName =
             "os2iot-docker_chirpstack-network-server_1:8000";
         const endpoint = "network-servers";
+        const createNetworkServerDto: CreateNetworkServerDto = {
+            name: chirpstackNetworkServerName,
+            server: chirpstackNetworkServerName,
+
+            caCert: "",
+            gatewayDiscoveryDR: 0,
+            gatewayDiscoveryEnabled: false,
+            gatewayDiscoveryInterval: 0,
+            gatewayDiscoveryTXFrequency: 0,
+            routingProfileCACert: "",
+            routingProfileTLSCert: "",
+            routingProfileTLSKey: "",
+            tlsCert: "",
+            tlsKey: "",
+        };
         const data: string = //TODO: skriv om til at bruge en DTO
-            '{"networkServer": { "name": "' +
-            chirpstackNetworkServerName +
-            '", "server": "' +
-            chirpstackNetworkServerName +
-            '"}}';
+            '{"networkServer":' + JSON.stringify(createNetworkServerDto) + "}";
 
         if ((await this.getCount(endpoint)) < 1) {
             this.post(endpoint, data); //Måske er det i virkeligheden ligegyldigt om den findes i forvejen. ??
