2efc8a7f0246ad04601f980b0eb078de86058d0f
Thomas Rohleder <tcr@netcompany.com>
2020-08-05 12:26:45 +0200

    IOT-88: changed chirpstack folder struckture

--- src/modules/device-integrations/chirpstack-administration.module.ts
+++ src/modules/device-integrations/chirpstack-administration.module.ts
@@ -1,5 +1,5 @@
 import { Module, HttpModule } from "@nestjs/common";
-import { ChirpstackSetupNetworkServerService } from "@services/chirpstack/setup-network-server/chirpstack-network-server.service";
+import { ChirpstackSetupNetworkServerService } from "@services/chirpstack/chirpstack-network-server.service";
 @Module({
     controllers: [],
     imports: [HttpModule],
