d2f1f2103e348362f8d5e08077e3bc1686655ab3
Troels Beck Krøgh <tbk@netcompany.com>
2020-08-07 10:23:10 +0200

    IOT-176: Add E2E tests for device integration persistence

--- test/e2e/test-helpers.ts
+++ test/e2e/test-helpers.ts
@@ -1,9 +1,80 @@
 import { getManager } from "typeorm";
+import { Application } from "@entities/application.entity";
+import { IoTDevice } from "@entities/iot-device.entity";
+import { GenericHTTPDevice } from "@entities/generic-http-device.entity";
+import { RawRequestDto } from "@entities/dto/kafka/raw-request.dto";
+import { KafkaPayload } from "../../src/services/kafka/kafka.message";
+import { KafkaTopic } from "../../src/entities/enum/kafka-topic.enum";
 
 export async function clearDatabase(): Promise<void> {
     await getManager().query(
         `DELETE FROM "iot_device"; \n` +
             `DELETE FROM "application"; \n` +
-            `DELETE FROM "data_target";`
+            `DELETE FROM "data_target"; \n` +
+            `DELETE FROM "received_message"; \n` +
+            `DELETE FROM "received_message_metadata";`
     );
+}
+
+export function generateApplication(): Application {
+    const app = new Application();
+    app.name = "E2E Test Application";
+    app.description = "E2E Test Application Description";
+    app.iotDevices = [];
+    app.dataTargets = [];
+
+    return app;
+}
+
+export async function generateSavedApplication(): Promise<Application> {
+    return await getManager().save(generateApplication());
+}
+
+export function generateIoTDevice(applications: Application): IoTDevice {
+    const device = new GenericHTTPDevice();
+    device.name = "E2E Test GENERIC HTTP device";
+    device.application = applications;
+    device.apiKey = "DUMMY-API-KEY";
+    device.metadata = JSON.parse('{"some_key": "a_value"}');
+
+    return device;
+}
+
+export async function generateSavedIoTDevice(
+    applications: Application
+): Promise<IoTDevice> {
+    return await getManager().save(generateIoTDevice(applications));
+}
+
+export function generateSigfoxRawRequstDto(
+    iotDeviceId?: number
+): RawRequestDto {
+    return {
+        rawPayload: JSON.parse(`{
+                "data": "c6099764",
+                "sigfoxId": "B445A9",
+                "time": "1596721546",
+                "snr": "12.53",
+                "rssi": "-123.00",
+                "avgSnr": "null",
+                "station": "37FF",
+                "seqNumber": "1",
+                "latStation": "null",
+                "lngStation": "null",
+                "ack": "false"
+            }`),
+        iotDeviceId: iotDeviceId || 1,
+        unixTimestamp: 1596721546,
+    };
+}
+
+export function generateRawRequestKafkaPayload(
+    iotDeviceId?: number
+): KafkaPayload {
+    return {
+        body: generateSigfoxRawRequstDto(iotDeviceId),
+        messageId: "genericHttp1596721546",
+        messageType: "receiveData.genericHttp",
+        topicName: KafkaTopic.RAW_REQUEST,
+    };
 }
--- test/unit/device-integration-persistence.service.spec.ts
+++ test/unit/device-integration-persistence.service.spec.ts
@@ -10,6 +10,7 @@ import { Application } from "@entities/application.entity";
 import { ApplicationService } from "@services/application.service";
 import { RawRequestDto } from "@entities/dto/kafka/raw-request.dto";
 import { IoTDeviceType } from "@enum/device-type.enum";
+import { generateSigfoxRawRequstDto, generateIoTDevice, generateApplication } from "../e2e/test-helpers";
 
 describe("DeviceIntegrationPersistenceService", () => {
     let service: DeviceIntegrationPersistenceService;
@@ -52,24 +53,6 @@ describe("DeviceIntegrationPersistenceService", () => {
         expect(service).toBeDefined();
     });
 
-    const sampleRawRequestDto = {
-        rawPayload: JSON.parse(`{
-                "data": "c6099764",
-                "sigfoxId": "B445A9",
-                "time": "1596721546",
-                "snr": "12.53",
-                "rssi": "-123.00",
-                "avgSnr": "null",
-                "station": "37FF",
-                "seqNumber": "1",
-                "latStation": "null",
-                "lngStation": "null",
-                "ack": "false"
-            }`),
-        iotDeviceId: 1,
-        unixTimestamp: 1596721546,
-    };
-
     const iotDevice: IoTDevice = {
         id: 1,
         application: {
@@ -90,10 +73,11 @@ describe("DeviceIntegrationPersistenceService", () => {
         receivedMessagesMetadata: [],
     };
 
-    it("test mapDtoToNewReceivedMessageMetadata - Sigfox data + with timestmap", () => {
+    it("test mapDtoToNewReceivedMessageMetadata - Sigfox data + with timestmap", async () => {
         // Arrange
-        const dto: RawRequestDto = sampleRawRequestDto;
-        const relatedIoTDevice: IoTDevice = iotDevice;
+        const dto: RawRequestDto = generateSigfoxRawRequstDto();
+        const application = generateApplication();
+        const relatedIoTDevice: IoTDevice = generateIoTDevice(application);
 
         // Act
         const metadata: ReceivedMessageMetadata = service.mapDtoToNewReceivedMessageMetadata(
@@ -102,13 +86,13 @@ describe("DeviceIntegrationPersistenceService", () => {
         );
 
         // Assert
-        expect(metadata.device.id).toBe(1);
-        expect(metadata.sentTime).toStrictEqual(new Date(sampleRawRequestDto.unixTimestamp));
+        expect(metadata.device).toMatchObject(relatedIoTDevice);
+        expect(metadata.sentTime).toStrictEqual(new Date(dto.unixTimestamp));
     });
 
     it("test mapDtoToReceivedMessage - Sigfox data + with timestmap", () => {
         // Arrange
-        const dto: RawRequestDto = sampleRawRequestDto;
+        const dto: RawRequestDto = generateSigfoxRawRequstDto();
         const existingMessage = new ReceivedMessage();
         const relatedIoTDevice: IoTDevice = iotDevice;
 
@@ -120,8 +104,8 @@ describe("DeviceIntegrationPersistenceService", () => {
         );
 
         // Assert
-        expect(message.device.id).toBe(1);
-        expect(message.sentTime).toStrictEqual(new Date(sampleRawRequestDto.unixTimestamp));
-        expect(message.rawData).toMatchObject(sampleRawRequestDto.rawPayload);
+        expect(message.device).toMatchObject(relatedIoTDevice);
+        expect(message.sentTime).toStrictEqual(new Date(dto.unixTimestamp));
+        expect(message.rawData).toMatchObject(dto.rawPayload);
     });
 });
