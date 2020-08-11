33f4598a202926f028ca9058e631d82e7f0f070c
Troels Beck Krøgh <tbk@netcompany.com>
2020-08-11 16:15:54 +0200

    Lint: Limit number of lines in functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,7 @@ module.exports = {
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-floating-promises": "warn",
+        "max-lines-per-function": ["error", 60],
+        "max-lines-per-function": ["warn", 30],
     },
 };
