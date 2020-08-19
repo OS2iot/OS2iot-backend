a11aace76f8e33178dc9820b8999e0e2b2e7ce91
Troels Beck Krøgh <tbk@netcompany.com>
2020-08-19 09:36:22 +0200

    IOT-28: Add getAll to CRUD

---
+++ src/entities/dto/list-all-payload-decoders-response.dto.ts
@@ -0,0 +1,6 @@
+import { ListAllEntitiesResponseDto } from "@dto/list-all-entities-reponse.dto";
+import { PayloadDecoder } from "@entities/payload-decoder.entity";
+
+export class ListAllPayloadDecoderReponseDto extends ListAllEntitiesResponseDto<
+    PayloadDecoder
+> {}
