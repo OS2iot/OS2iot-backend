371bec36eabcfb9673b62e536b0dd36019d0c90d
Troels Beck Krøgh <tbk@netcompany.com>
2020-08-19 09:36:22 +0200

    IOT-28: Add getAll to CRUD

--- src/services/payload-decoder.service.ts
+++ src/services/payload-decoder.service.ts
@@ -4,6 +4,8 @@ import { Repository, DeleteResult } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CreatePayloadDecoderDto } from "@dto/create-payload-decoder.dto";
 import { UpdatePayloadDecoderDto } from "@dto/update-payload-decoder.dto";
+import { ListAllEntitiesDto } from "@dto/list-all-entities.dto";
+import { ListAllPayloadDecoderReponseDto } from "@dto/list-all-payload-decoders-response.dto";
 
 @Injectable()
 export class PayloadDecoderService {
@@ -14,6 +16,25 @@ export class PayloadDecoderService {
 
     async findOne(id: number): Promise<PayloadDecoder> {
         return await this.payloadDecoderRepository.findOneOrFail(id);
+    }
+
+    async findAndCountWithPagination(
+        query: ListAllEntitiesDto
+    ): Promise<ListAllPayloadDecoderReponseDto> {
+        const [
+            result,
+            total,
+        ] = await this.payloadDecoderRepository.findAndCount({
+            where: {},
+            take: query.limit,
+            skip: query.offset,
+            order: { id: query.sort }, // TODO: Generic sorting possible?
+        });
+
+        return {
+            data: result,
+            count: total,
+        };
     }
 
     async create(createDto: CreatePayloadDecoderDto): Promise<PayloadDecoder> {
