0283d8893b7b5f47fc73784f850fb7862591761f
Troels Beck Krøgh <tbk@netcompany.com>
2020-09-08 10:21:52 +0200

    IOT-44: Initial Local + JWT security

--- src/modules/default.module.ts
+++ src/modules/default.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { DefaultController } from "@admin-controller/default.controller";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
+    imports: [AuthModule],
     controllers: [DefaultController],
 })
 export class DefaultModule {}
