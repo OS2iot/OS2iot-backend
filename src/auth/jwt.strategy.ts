31dce62f98540347258f1056ae68e0d0cd126eed
Troels Beck Krøgh <tbk@netcompany.com>
2020-09-14 12:54:35 +0200

    IOT-44: Check if user exists, not just JWT is valid

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,14 +1,18 @@
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { PassportStrategy } from "@nestjs/passport";
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { jwtConstants } from "./constants";
 import { PermissionService } from "@services/user-management/permission.service";
 import { JwtPayloadDto } from "@entities/dto/internal/jwt-payload.dto";
 import { AuthenticatedUser } from "@dto/internal/authenticated-user";
+import { UserService } from "../services/user-management/user.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private permissionService: PermissionService) {
+    constructor(
+        private permissionService: PermissionService,
+        private userService: UserService
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
@@ -17,6 +21,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: JwtPayloadDto): Promise<AuthenticatedUser> {
+        // Does the user still exist?
+        const user = await this.userService.findOne(payload.sub);
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
         // This data is already validated
         const permissions = await this.permissionService.findPermissionGroupedByLevelForUser(
             payload.sub
