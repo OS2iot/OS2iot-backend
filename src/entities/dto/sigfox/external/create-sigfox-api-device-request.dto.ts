7dc3b94fc34f34dd4fe5f8d304e71a0d00231cc6
Troels Beck Krøgh <tbk@netcompany.com>
2020-10-05 11:37:47 +0200

    IOT-53: Add SigFoxApiDeviceService and DTOs

---
+++ src/entities/dto/sigfox/external/create-sigfox-api-device-request.dto.ts
@@ -0,0 +1,17 @@
+export class CreateSigFoxApiDeviceRequestDto {
+    id: string;
+    name: string;
+    pac: string;
+    deviceTypeId: string;
+
+    activable?: boolean;
+    automaticRenewal?: boolean;
+    lat?: number;
+    lng?: number;
+    productCertificate?: ProductCertificate;
+    prototype?: boolean;
+}
+
+export class ProductCertificate {
+    key: string;
+}
---
+++ src/entities/dto/sigfox/external/sigfox-api-device-response.dto.ts
@@ -0,0 +1,46 @@
+import { SigFoxApiIdReferenceDto } from "./sigfox-api-id-reference.dto";
+
+export interface SigFoxApiDeviceResponse {
+    data: SigFoxApiDeviceContent[];
+}
+
+export interface SigFoxApiDeviceContent {
+    id: string;
+    name: string;
+    satelliteCapable: boolean;
+    sequenceNumber: number;
+    lastCom: any;
+    state: number;
+    comState: number;
+    pac: string;
+    location: Location;
+    deviceType: SigFoxApiIdReferenceDto;
+    group: SigFoxApiIdReferenceDto;
+    lqi: number;
+    activationTime: any;
+    token: Token;
+    contract: SigFoxApiIdReferenceDto;
+    creationTime: any;
+    modemCertificate: SigFoxApiIdReferenceDto;
+    prototype: boolean;
+    productCertificate: SigFoxApiIdReferenceDto;
+    automaticRenewal: boolean;
+    automaticRenewalStatus: number;
+    createdBy: string;
+    lastEditionTime: any;
+    lastEditedBy: string;
+    activable: boolean;
+}
+
+export interface Location {
+    lat: number;
+    lng: number;
+}
+
+export interface Token {
+    state: number;
+    detailMessage: string;
+    end: any;
+    freeMessages?: number;
+    freeMessagesSent?: number;
+}
