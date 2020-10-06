bc8037bd1ae04c60c65303031b6fd7286739ce0a
Troels Beck Krøgh <tbk@netcompany.com>
2020-10-06 08:45:26 +0200

    Make Chirpstack JWT valid 10 seconds before generating to avoid synchronization issues

--- src/services/chirpstack/jwt-token.ts
+++ src/services/chirpstack/jwt-token.ts
@@ -9,7 +9,8 @@ export class JwtToken {
         const claims = {
             iss: "chirpstack-application-server", // issuer of the claim
             aud: "chirpstack-application-server", // audience for which the claim is intended
-            nbf: Math.floor(new Date().valueOf() / 1000), // unix time from which the token is valid
+            iat: Math.floor(new Date().valueOf() / 1000 - 10), // unix time from which the token is valid
+            nbf: Math.floor(new Date().valueOf() / 1000 - 10), // unix time from which the token is valid
             exp: Math.floor(new Date().valueOf() / 1000) + 60 * 60 * 24 * 14, // unix time when the token expires
             sub: "user", // subject of the claim (an user)
             username: "admin", // username the client claims to be
