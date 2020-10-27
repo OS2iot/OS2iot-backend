89863ac21797ce04aed970ff701f14ed0556ceb3
Troels Beck Krøgh <tbk@netcompany.com>
2020-10-27 14:48:20 +0100

    IOT-43: Use localhost

--- src/config/passport-saml.conf.ts
+++ src/config/passport-saml.conf.ts
@@ -4,11 +4,11 @@ import { SamlConfig } from "passport-saml";
 /**
  *  https://github.com/bergie/passport-saml
  */
-const BASE_URL = "https://8470fd5e996d.ngrok.io";
+const BASE_URL = "https://localhost:8443";
 
 export const samlPassportConf: SamlConfig = {
     // audience: BASEURL + "",
-    issuer: "https://8470fd5e996d.ngrok.io/api/v1/auth/kombit/metadata", // `${BASE_URL}/api/v1/auth/kombit/metadata`,
+    issuer: "https://localhost:8443/api/v1/auth/kombit/metadata", // `${BASE_URL}/api/v1/auth/kombit/metadata`,
 
     identifierFormat: "", // "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName",
 
@@ -26,3 +26,4 @@ export const samlPassportConf: SamlConfig = {
     // RACComparison: "minimum",
     // passive: false,
 };
+    
