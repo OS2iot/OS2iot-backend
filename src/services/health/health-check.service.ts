f950abad159054c5013f3b0c17c9950c10d0b6a9
Troels Beck Krøgh <tbk@netcompany.com>
2020-11-27 14:35:56 +0100

    Log healthcheck errors

--- src/services/health/health-check.service.ts
+++ src/services/health/health-check.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 
 @Injectable()
 export class HealthCheckService {
+    private readonly logger = new Logger(HealthCheckService.name);
+
     // start at startup time ...
     public lastHeartbeat: number = new Date().valueOf();
 
     private readonly MAX_HEARTBEAT_DELAY = 60000;
 
     isKafkaOk(): boolean {
-        return new Date().valueOf() - this.lastHeartbeat <= this.MAX_HEARTBEAT_DELAY;
+        const isOk =
+            new Date().valueOf() - this.lastHeartbeat <= this.MAX_HEARTBEAT_DELAY;
+        if (!isOk) {
+            this.logger.error(
+                `Healthcheck not OK. Last heartbeat from Kafka was at ${this.lastHeartbeat}`
+            );
+        }
+        return isOk;
     }
 }
