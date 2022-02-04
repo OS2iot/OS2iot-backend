35934be3ea8c4e0d3f5a16c4e569b4eaf66d6149
Aram Al-Sabti <afa@iterator-it.dk>
2022-02-04 10:58:49 +0100

    Remove restriction on devices belonging to the same application

--- src/entities/dto/iot-device/create-iot-device-batch.dto.ts
+++ src/entities/dto/iot-device/create-iot-device-batch.dto.ts
@@ -1,13 +1,8 @@
 import { CreateIoTDeviceDto } from "@dto/create-iot-device.dto";
-import { HasSameApplicationId } from "@helpers/has-same-application-id.validator";
-import { nameof } from "@helpers/type-helper";
 import { ArrayMaxSize, ArrayNotEmpty } from "class-validator";
 
 export class CreateIoTDeviceBatchDto {
     @ArrayNotEmpty()
     @ArrayMaxSize(50)
-    @HasSameApplicationId(nameof<CreateIoTDeviceBatchDto>("data"), {
-        applicationIdName: nameof<CreateIoTDeviceDto>("applicationId"),
-    })
     data: CreateIoTDeviceDto[];
 }
