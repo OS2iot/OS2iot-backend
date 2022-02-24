9f2a8c601f18761532cba1fdd45589ef2ea06591
Aram Al-Sabti <afa@iterator-it.dk>
2022-02-24 14:39:45 +0100

    Init MQTT auth

--- src/services/data-management/chirpstack-mqtt-listener.service.ts
+++ src/services/data-management/chirpstack-mqtt-listener.service.ts
@@ -6,15 +6,21 @@ import { ChirpstackMQTTMessageDto } from "@dto/chirpstack/chirpstack-mqtt-messag
 import { IoTDeviceType } from "@enum/device-type.enum";
 import { ReceiveDataService } from "@services/data-management/receive-data.service";
 import { IoTDeviceService } from "@services/device-management/iot-device.service";
+import configuration from "@config/configuration";
 
 @Injectable()
 export class ChirpstackMQTTListenerService implements OnApplicationBootstrap {
     constructor(
         private receiveDataService: ReceiveDataService,
         private iotDeviceService: IoTDeviceService
-    ) {}
+    ) {
+        this.username = configuration()["chirpstack"]["username"];
+        this.password = configuration()["chirpstack"]["password"];
+    }
 
     private readonly logger = new Logger(ChirpstackMQTTListenerService.name);
+    private readonly username: string;
+    private readonly password: string;
 
     MQTT_CLIENT_ID = "os2iot-backend";
     MQTT_URL = `mqtt://${process.env.CS_MQTT_HOSTNAME || "localhost"}:${
@@ -30,6 +36,8 @@ export class ChirpstackMQTTListenerService implements OnApplicationBootstrap {
         this.client = mqtt.connect(this.MQTT_URL, {
             clean: true,
             clientId: this.MQTT_CLIENT_ID,
+            username: this.username,
+            password: this.password,
         });
         this.client.on("connect", () => {
             this.client.subscribe(this.CHIRPSTACK_MQTT_DEVICE_DATA_TOPIC);
