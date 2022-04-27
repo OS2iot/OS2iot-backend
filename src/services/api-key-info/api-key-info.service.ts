c03a1ef5ea61d5aab8bc31b58c24b8e546434ab3
AramAlsabti <92869496+AramAlsabti@users.noreply.github.com>
2022-04-27 11:42:22 +0200

    Add controller for retrieving API key info (#162)
    * Add controller for api key info like GET organization
    
    * Fix build errors

---
+++ src/services/api-key-info/api-key-info.service.ts
@@ -0,0 +1,17 @@
+import { Organization } from "@entities/organization.entity";
+import { forwardRef, Inject, Injectable, Logger } from "@nestjs/common";
+import { OrganizationService } from "@services/user-management/organization.service";
+
+@Injectable()
+export class ApiKeyInfoService {
+    constructor(
+        @Inject(forwardRef(() => OrganizationService))
+        private organizationService: OrganizationService
+    ) {}
+
+    private readonly logger = new Logger(ApiKeyInfoService.name, true);
+
+    findOrganization(orgId: number): Promise<Organization> {
+        return this.organizationService.findById(orgId);
+    }
+}
