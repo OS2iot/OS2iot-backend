692e4c3452a1b50751a2939847081c2adb439cd4
Aram Al-Sabti <afa@iterator-it.dk>
2022-05-18 10:24:46 +0200

    Bump migration timestamp

--- src/migration/1652861431000-gateway-status-history.ts
+++ src/migration/1652861431000-gateway-status-history.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class gatewayStatusHistory1652709489000 implements MigrationInterface {    
-    name = 'gatewayStatusHistory1652709489000'
+export class gatewayStatusHistory1652861431000 implements MigrationInterface {
+    name = 'gatewayStatusHistory1652861431000'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "gateway_status_history" ("id" SERIAL NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "mac" character varying NOT NULL, "wasOnline" boolean NOT NULL, "timestamp" TIMESTAMP NOT NULL DEFAULT now(), "createdById" integer, "updatedById" integer, CONSTRAINT "UQ_5370e37f34adf6e9c9350bc46c7" UNIQUE ("mac", "timestamp"), CONSTRAINT "PK_defafcb0f6f1ba7a612395b62f8" PRIMARY KEY ("id"))`);
