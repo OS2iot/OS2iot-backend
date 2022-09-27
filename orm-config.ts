a3b8db4fef5f3546655a96f70830dc40e2d94195
Aram Al-Sabti <afa@iterator-it.dk>
2022-09-27 15:20:36 +0200

    fix: typeorm migrations

---
+++ orm-config.ts
@@ -0,0 +1,22 @@
+import { DataSource } from "typeorm";
+
+const dataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DATABASE_HOSTNAME || "host.docker.internal",
+    port: parseInt(process.env.DATABASE_PORT ?? "", 10) || 5433,
+    username: process.env.DATABASE_USERNAME || "os2iot",
+    password: process.env.DATABASE_PASSWORD || "toi2so",
+    database: "os2iot",
+    synchronize: false,
+    logging: false,
+    // For some reason, entities path is from where "typeorm" is executed from console.
+    // TypeORM commands are executed from the root folder.
+    entities: ["src/entities/*.ts", "src/entities/permissions/*.ts"],
+    // From v3, it's no longer used as output path...
+    migrations: ["src/migration/*.ts"],
+});
+
+/**
+ * For CLI migration use only
+ */
+export default dataSource;
