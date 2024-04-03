0753c76946629cc93b0cbd16a9924efe81ce49e5
Frederik Christ Vestergaard <fcv@iterator-it.dk>
2024-04-03 12:17:46 +0200

    Updated backend for new gateway fields

--- src/entities/dto/chirpstack/gateway-contents.dto.ts
+++ src/entities/dto/chirpstack/gateway-contents.dto.ts
@@ -1,9 +1,12 @@
 import { ApiHideProperty, ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import {
+    IsEmail,
+    IsEnum,
     IsHexadecimal,
     IsJSON,
     IsOptional,
+    IsPhoneNumber,
     IsString,
     Length,
     Matches,
@@ -12,6 +15,7 @@ import {
     ValidateNested,
 } from "class-validator";
 import { CommonLocationDto } from "@dto/chirpstack/common-location.dto";
+import { GatewayPlacement, GatewayStatus } from "@enum/gateway.enum";
 
 export class GatewayContentsDto {
     @ApiProperty({ required: true })
@@ -46,6 +50,54 @@ export class GatewayContentsDto {
         message: "The name may only contain words, numbers and dashes.",
     })
     name: string;
+
+    @ApiProperty({ required: false })
+    @IsEnum(GatewayPlacement)
+    @IsOptional()
+    placement?: GatewayPlacement;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    modelName?: string;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    antennaType?: string;
+
+    @ApiProperty({ required: false })
+    @IsEnum(GatewayStatus)
+    @IsOptional()
+    status?: GatewayStatus;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    gatewayResponsibleName?: string;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    @IsEmail()
+    gatewayResponsibleEmail?: string;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    @IsPhoneNumber("DK")
+    gatewayResponsiblePhoneNumber?: string;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    operationalResponsibleName?: string;
+
+    @ApiProperty({ required: false })
+    @IsString()
+    @IsOptional()
+    @IsEmail()
+    operationalResponsibleEmail?: string;
 
     @ApiHideProperty()
     tenantId: string;
---
+++ src/entities/enum/gateway.enum.ts
@@ -0,0 +1,12 @@
+﻿export enum GatewayPlacement {
+    Indoors = "INDOORS",
+    Outdoors = "OUTDOORS",
+    Other = "OTHER",
+}
+
+export enum GatewayStatus {
+    InUse = "IN-USE",
+    Project = "PROJECT",
+    Prototype = "PROTOTYPE",
+    Other = "OTHER",
+}
