0bed69fef721ce62745100aaa11dfb59e67d590a
Chris Dons Johansen <cdj@iterator-it.dk>
2024-09-24 11:50:50 +0200

    Fixed missing auth on new DatatargetLog-controller..

--- src/controllers/admin-controller/data-target-log.controller.ts
+++ src/controllers/admin-controller/data-target-log.controller.ts
@@ -1,11 +1,20 @@
+import { ComposeAuthGuard } from "@auth/compose-auth.guard";
+import { Read } from "@auth/roles.decorator";
+import { RolesGuard } from "@auth/roles.guard";
+import { ApiAuth } from "@auth/swagger-auth-decorator";
 import { DatatargetLog } from "@entities/datatarget-log.entity";
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Controller, Get, Param, ParseIntPipe, UseGuards } from "@nestjs/common";
+import { ApiForbiddenResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
 @ApiTags("Data Target Logs")
 @Controller("datatarget-log")
+@UseGuards(ComposeAuthGuard, RolesGuard)
+@ApiAuth()
+@Read()
+@ApiForbiddenResponse()
+@ApiUnauthorizedResponse()
 export class DatatargetLogController {
   constructor(
     @InjectRepository(DatatargetLog)
